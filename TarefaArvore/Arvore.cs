5c27e9ea1591eadba41b5cb65e83f0c5e82a7817
Deullam <deullam@gmail.com>
2021-07-01 03:31:01 -0300

    feat: Conclusão da tarefa 2

---
+++ TarefaArvore/Arvore.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TarefaArvore
+{
+    public class Arvore
+    {
+        public int Raiz { get; set; }
+        public List<int> GalhoEsquerda { get; set; }
+        public List<int> GalhoDireita { get; set; }
+
+
+        public Arvore()
+        {
+            Raiz = 0;
+            GalhoEsquerda = new List<int>();
+            GalhoDireita = new List<int>();
+        }
+
+        /** Método utilizado para criar a estrutura da Arvore conforme array */
+        public void CriarArvore(int[] lista)
+        {
+            int MaiorValor = 0;
+            int indice = 0;
+            foreach (var numero in lista)
+            {
+                if (numero > MaiorValor)
+                {
+                    MaiorValor = numero;
+                    indice = Array.IndexOf(lista, numero);
+                }
+            }
+            Raiz = MaiorValor;
+
+            for (int i = 0; i < indice; i++)
+            {
+                GalhoEsquerda.Add(lista[i]);
+            }
+            GalhoEsquerda.Sort();
+            GalhoEsquerda.Reverse();
+           
+            indice++;
+            for (int i = indice; i < lista.Length; i++)
+            {
+                GalhoDireita.Add(lista[i]);
+            }
+            GalhoDireita.Sort();
+            GalhoDireita.Reverse();
+        }
+
+        /** Metodo para mostrar a Arvore no console*/
+        public void MostrarArvore()
+        {
+            Console.WriteLine("Raiz: " + Raiz);
+            Console.Write("Galho esquerda: ");
+            foreach (var numero in GalhoEsquerda)
+            {
+                Console.Write(" " + numero);
+            }
+
+            Console.Write("\nGalho direita: ");
+            foreach (var numero in GalhoDireita)
+            {
+                Console.Write(" " + numero);
+            }
+        }
+    }
+}
---
+++ TarefaArvore/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TarefaArvore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] lista = new int[6] { 3, 2, 1, 6, 0, 5 };
+            int[] lista2 = new int[7] { 7, 5, 13, 9, 1, 6, 4 };
+
+
+            Arvore arvore = new();
+            Arvore arvore2 = new();
+
+            arvore.CriarArvore(lista);
+            arvore2.CriarArvore(lista2);
+
+            Console.WriteLine("Árvore 1: ");
+            arvore.MostrarArvore();
+
+            Console.WriteLine("\n \nÁrvore 2: ");
+            arvore2.MostrarArvore();
+
+        }
+    }
+}
